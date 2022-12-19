c8dc438343ea082e52702851087baf1d45057604
Steve Kirbach <steven.kirbach@gmail.com>
2022-12-19 13:36:00 -0800

    moving test component files

--- tests/test_component/Source/CWinRT/shim.c
+++ tests/test_component/Source/CWinRT/shim.c
@@ -4,23 +4,3 @@
 // linking, at least the way our CMakeLists.txt are currently structured, we
 // also need to export a symbol.
 extern unsigned _;
-
-// ISO C requires a translation unit to contain at least one declaration.
-//
-// To force the linker to produce a .lib file, currently required for dynamic
-// linking, at least the way our CMakeLists.txt are currently structured, we
-// also need to export a symbol.
-#if defined(Ctest_component_EXPORTS)
-  #define CWINRT_ABI __declspec(dllexport)
-#else
-  #define CWINRT_ABI __declspec(dllimport)
-#endif
-
-// ISO C requires a translation unit to contain at least one declaration.
-//
-// To force the linker to produce a .lib file, currently required for dynamic
-// linking, at least the way our CMakeLists.txt are currently structured, we
-// also need to export a symbol.
-int CWINRT_ABI force_lib_generation(void) {
-  return 1;
-}
