d9aed1705b29a41e3f07f28615c9a200764e6d2a
Steve Kirbach <steven.kirbach@gmail.com>
2023-06-15 21:22:21 -0700

    don't delete project files written on disk (#61)

--- swiftwinrt/Resources/Support/IUnknown.swift
+++ swiftwinrt/Resources/Support/IUnknown.swift
@@ -7,7 +7,7 @@ import C_BINDINGS_MODULE
 open class IUnknown : HasIID {
   public let pUnk: IUnknownRef
 
-  public class var IID: IID { IID_IUnknown }
+  open class var IID: IID { IID_IUnknown }
 
   public required init(_ pointer: UnsafeMutablePointer<WinSDK.IUnknown>) {
     self.pUnk = IUnknownRef(pointer)
