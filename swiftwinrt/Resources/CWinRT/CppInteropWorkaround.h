69184080f679239fc37e6cd66bca476479139b30
Tristan Labelle <tristan@thebrowser.company>
2023-09-27 15:16:41 -0400

    Replace the Windows API IVectorChangedEventArgs.h to avoid ambiguities when referencing from generated code.

--- swiftwinrt/Resources/CWinRT/CppInteropWorkaround.h
+++ swiftwinrt/Resources/CWinRT/CppInteropWorkaround.h
@@ -82,9 +82,9 @@ inline HRESULT RoActivateInstance_Workaround(
 //-------------------------------------------------------------------------------------------------
 // Preemptively include headers before swapping out the IID type
 // This no-op the #includes in generated code, so that it instead relies of our workaround types
+#include <inspectable.h>
 #include <EventToken.h>
 #include <windowscontracts.h>
-#include <ivectorchangedeventargs.h>
 
 #define GUID GUID_Workaround
 #undef REFGUID
@@ -101,8 +101,6 @@ inline HRESULT RoActivateInstance_Workaround(
 #define IUnknownVtbl IUnknownVtbl_Workaround
 #define IInspectable IInspectable_Workaround
 #define IInspectableVtbl IInspectableVtbl_Workaround
-#define __x_ABI_CWindows_CFoundation_CCollections_CIVectorChangedEventArgs __x_ABI_CWindows_CFoundation_CCollections_CIVectorChangedEventArgs_Workaround
-#define __x_ABI_CWindows_CFoundation_CCollections_CIVectorChangedEventArgsVtbl __x_ABI_CWindows_CFoundation_CCollections_CIVectorChangedEventArgsVtbl_Workaround
 
 // iunknown.h
 typedef struct IUnknownVtbl
@@ -161,66 +159,4 @@ typedef struct IInspectableVtbl
 interface IInspectable
 {
     CONST_VTBL struct IInspectableVtbl *lpVtbl;
-};
-
-// ivectorchangedeventargs.h
-typedef interface __x_ABI_CWindows_CFoundation_CCollections_CIVectorChangedEventArgs __x_ABI_CWindows_CFoundation_CCollections_CIVectorChangedEventArgs;
-
-#ifdef __cplusplus
-// We rely on the C version of the enum
-typedef ABI::Windows::Foundation::Collections::CollectionChange __x_ABI_CWindows_CFoundation_CCollections_CCollectionChange;
-#endif
-
-/* [contract, version, pointer_default(unique), uuid("575933df-34fe-4480-af15-07691f3d5d9b"), object] */
-typedef struct __x_ABI_CWindows_CFoundation_CCollections_CIVectorChangedEventArgsVtbl
-{
-    BEGIN_INTERFACE
-
-    HRESULT ( STDMETHODCALLTYPE *QueryInterface)(
-        __RPC__in __x_ABI_CWindows_CFoundation_CCollections_CIVectorChangedEventArgs * This,
-        /* [in] */ __RPC__in REFIID riid,
-        /* [annotation][iid_is][out] */
-        _COM_Outptr_  void **ppvObject
-        );
-
-    ULONG ( STDMETHODCALLTYPE *AddRef )(
-        __RPC__in __x_ABI_CWindows_CFoundation_CCollections_CIVectorChangedEventArgs * This
-        );
-
-    ULONG ( STDMETHODCALLTYPE *Release )(
-        __RPC__in __x_ABI_CWindows_CFoundation_CCollections_CIVectorChangedEventArgs * This
-        );
-
-    HRESULT ( STDMETHODCALLTYPE *GetIids )(
-        __RPC__in __x_ABI_CWindows_CFoundation_CCollections_CIVectorChangedEventArgs * This,
-        /* [out] */ __RPC__out ULONG *iidCount,
-        /* [size_is][size_is][out] */ __RPC__deref_out_ecount_full_opt(*iidCount) IID **iids
-        );
-
-    HRESULT ( STDMETHODCALLTYPE *GetRuntimeClassName )(
-        __RPC__in __x_ABI_CWindows_CFoundation_CCollections_CIVectorChangedEventArgs * This,
-        /* [out] */ __RPC__deref_out_opt HSTRING *className
-        );
-
-    HRESULT ( STDMETHODCALLTYPE *GetTrustLevel )(
-        __RPC__in __x_ABI_CWindows_CFoundation_CCollections_CIVectorChangedEventArgs * This,
-        /* [OUT ] */ __RPC__out TrustLevel *trustLevel
-        );
-
-    /* [propget] */HRESULT ( STDMETHODCALLTYPE *get_CollectionChange )(
-        __x_ABI_CWindows_CFoundation_CCollections_CIVectorChangedEventArgs * This,
-        /* [retval, out] */__RPC__out __x_ABI_CWindows_CFoundation_CCollections_CCollectionChange * value
-        );
-
-    /* [propget] */HRESULT ( STDMETHODCALLTYPE *get_Index )(
-        __x_ABI_CWindows_CFoundation_CCollections_CIVectorChangedEventArgs * This,
-        /* [retval, out] */__RPC__out unsigned int * value
-        );
-
-    END_INTERFACE
-} __x_ABI_CWindows_CFoundation_CCollections_CIVectorChangedEventArgsVtbl;
-
-interface __x_ABI_CWindows_CFoundation_CCollections_CIVectorChangedEventArgs
-{
-    CONST_VTBL struct __x_ABI_CWindows_CFoundation_CCollections_CIVectorChangedEventArgsVtbl *lpVtbl;
 };
