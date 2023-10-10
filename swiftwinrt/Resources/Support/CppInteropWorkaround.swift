92075e3d7729aaca5d4b5fcf94792a7adc431ca5
Tristan Labelle <github@tristanlabelle.com>
2023-10-10 13:42:23 -0700

    Test disabling the C++ interop workaround for wchar_t

--- swiftwinrt/Resources/Support/CppInteropWorkaround.swift
+++ swiftwinrt/Resources/Support/CppInteropWorkaround.swift
@@ -11,19 +11,11 @@ public typealias REFIID = UnsafePointer<C_BINDINGS_MODULE.IID_Workaround>
 public typealias C_IUnknown = C_BINDINGS_MODULE.IUnknown_Workaround
 public typealias C_IInspectable = C_BINDINGS_MODULE.IInspectable_Workaround
 public typealias C_IInspectableVtbl = C_BINDINGS_MODULE.IInspectableVtbl_Workaround
-internal typealias IRestrictedErrorInfo = C_BINDINGS_MODULE.IRestrictedErrorInfo_Workaround
 internal let CoCreateInstance = C_BINDINGS_MODULE.CoCreateInstance_Workaround
-internal let GetRestrictedErrorInfo = C_BINDINGS_MODULE.GetRestrictedErrorInfo_Workaround
-internal let FormatMessageW = C_BINDINGS_MODULE.FormatMessageW_Workaround
 internal let UuidFromStringA = C_BINDINGS_MODULE.UuidFromStringA_Workaround
 internal let RoActivateInstance = C_BINDINGS_MODULE.RoActivateInstance_Workaround
 internal let RoGetActivationFactory = C_BINDINGS_MODULE.RoGetActivationFactory_Workaround
 internal let StringFromGUID2 = C_BINDINGS_MODULE.StringFromGUID2_Workaround
-internal let SysAllocString = C_BINDINGS_MODULE.SysAllocString_Workaround
-internal let SysFreeString = C_BINDINGS_MODULE.SysFreeString_Workaround
-internal let SysStringLen = C_BINDINGS_MODULE.SysStringLen_Workaround
-internal let WindowsCreateString = C_BINDINGS_MODULE.WindowsCreateString_Workaround
-internal let WindowsGetStringRawBuffer = C_BINDINGS_MODULE.WindowsGetStringRawBuffer_Workaround
 #else
 public typealias WCHAR = C_BINDINGS_MODULE.WCHAR
 public typealias BSTR = C_BINDINGS_MODULE.BSTR
@@ -35,17 +27,18 @@ public typealias REFIID = UnsafePointer<C_BINDINGS_MODULE.IID>
 public typealias C_IUnknown = C_BINDINGS_MODULE.IUnknown
 public typealias C_IInspectable = C_BINDINGS_MODULE.IInspectable
 public typealias C_IInspectableVtbl = C_BINDINGS_MODULE.IInspectableVtbl
-internal typealias IRestrictedErrorInfo = C_BINDINGS_MODULE.IRestrictedErrorInfo
 internal let CoCreateInstance = C_BINDINGS_MODULE.CoCreateInstance
-internal let GetRestrictedErrorInfo = C_BINDINGS_MODULE.GetRestrictedErrorInfo
-internal let FormatMessageW = C_BINDINGS_MODULE.FormatMessageW
 internal let UuidFromStringA = C_BINDINGS_MODULE.UuidFromStringA
 internal let RoActivateInstance = C_BINDINGS_MODULE.RoActivateInstance
 internal let RoGetActivationFactory = C_BINDINGS_MODULE.RoGetActivationFactory
 internal let StringFromGUID2 = C_BINDINGS_MODULE.StringFromGUID2
+#endif
+
+internal typealias IRestrictedErrorInfo = C_BINDINGS_MODULE.IRestrictedErrorInfo
+internal let GetRestrictedErrorInfo = C_BINDINGS_MODULE.GetRestrictedErrorInfo
+internal let FormatMessageW = C_BINDINGS_MODULE.FormatMessageW
 internal let SysAllocString = C_BINDINGS_MODULE.SysAllocString
 internal let SysFreeString = C_BINDINGS_MODULE.SysFreeString
 internal let SysStringLen = C_BINDINGS_MODULE.SysStringLen
 internal let WindowsCreateString = C_BINDINGS_MODULE.WindowsCreateString
 internal let WindowsGetStringRawBuffer = C_BINDINGS_MODULE.WindowsGetStringRawBuffer
-#endif
