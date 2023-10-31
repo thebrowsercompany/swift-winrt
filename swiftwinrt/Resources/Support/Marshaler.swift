e55db1d8200a901625afe085f67ebafda31d4269
Steve Kirbach <steven.kirbach@gmail.com>
2023-10-31 11:21:42 -0700

    pr feedback

--- swiftwinrt/Resources/Support/Marshaler.swift
+++ swiftwinrt/Resources/Support/Marshaler.swift
@@ -1,13 +1,11 @@
 import WinSDK
 import C_BINDINGS_MODULE
 
-extension IUnknownRef {
-    func copyTo(_ riid: UnsafeMutablePointer<IID_Workaround>?, _ ppvObj: UnsafeMutablePointer<LPVOID?>?) -> HRESULT {
+fileprivate extension IUnknownRef {
+    func copyTo(_ riid: UnsafeMutablePointer<SUPPORT_MODULE.IID>?, _ ppvObj: UnsafeMutablePointer<LPVOID?>?) -> HRESULT {
         self.borrow.pointee.lpVtbl.pointee.QueryInterface(self.borrow, riid, ppvObj)
     }
 }
-
-var IMarshal_IID: IID = IID(Data1: 0x01000300, Data2: 0x0000, Data3: 0x0000, Data4: (0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46)) // 01000300-0000-0000-C000-00000046
 
 func makeMarshaler(_ outer: IUnknownRef, _ result: UnsafeMutablePointer<UnsafeMutableRawPointer?>) throws {
     let marshaler = try Marshaler(outer)
@@ -33,11 +31,11 @@ fileprivate class IMarshalBridge: AbiBridge {
     typealias SwiftProjection = Marshaler
 }
 private var IMarshalVTable: IMarshalVtbl = .init(
-    QueryInterface: {
-        guard let pUnk = $0, let riid = $1, let ppvObject = $2 else { return E_INVALIDARG }
-        return riid.withMemoryRebound(to: IID_Workaround.self, capacity: 1) {
+    QueryInterface: { pUnk, riid, ppvObject in
+        guard let pUnk, let riid, let ppvObject else { return E_INVALIDARG }
+        return riid.withMemoryRebound(to: SUPPORT_MODULE.IID.self, capacity: 1) {
             switch $0.pointee {
-            case IMarshal_IID:
+            case unsafeBitCast(IID_IMarshal, to: SUPPORT_MODULE.IID.self):
                 _ = pUnk.pointee.lpVtbl.pointee.AddRef(pUnk)
                 return S_OK
             default:
