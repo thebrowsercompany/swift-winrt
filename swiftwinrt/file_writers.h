2d17dc5371f2b361ece201bc5f280b4bc1b724b1
Steven <steven.kirbach@gmail.com>
2025-04-11 16:39:07 -0700

    remove backticks

--- swiftwinrt/file_writers.h
+++ swiftwinrt/file_writers.h
@@ -274,7 +274,7 @@ namespace swiftwinrt
             w.swap();
             write_preamble(w, /* swift_code: */ true);
 
-            w.save_file(member.get().swift_type_name());
+            w.save_file(remove_backtick(member.get().swift_type_name())); // remove backtick for generic interfaces
         }   
     }
 
@@ -337,7 +337,7 @@ namespace swiftwinrt
             w.swap();
             write_preamble(w, /* swift_code: */ true);
 
-            w.save_file(member.get().swift_type_name());
+            w.save_file(remove_backtick(member.get().swift_type_name())); // remove backtick for generic delegates
         }   
     }
 
